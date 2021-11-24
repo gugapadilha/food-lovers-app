544cfe69387ee84dc75721366f0971dd621114bf
gugapadilha <guga.santospadilha@rede.ulbra.br>
2021-11-24 15:59:12 -0300

    impPizzaLayout

--- app/src/main/java/com/example/foodlovers/CardapioActivity.kt
+++ app/src/main/java/com/example/foodlovers/CardapioActivity.kt
@@ -1,11 +1,17 @@
 package com.example.foodlovers
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_cardapio.*
 
 class CardapioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cardapio)
+
+        cv_pizza.setOnClickListener {
+            startActivity(Intent(this@CardapioActivity, PizzaActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/example/foodlovers/PizzaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.foodlovers
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PizzaActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pizza)
+    }
+}
